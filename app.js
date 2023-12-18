1ea959a59b95895b5145deab9e0760227f47a663
anshita <vermaanshita2003@gmail.com>
2023-12-18 22:36:04 +0530

    mdb connected

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB!!!');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-app.get('/',(req,res)=>{
-    res.send('Hello!')
-})
+connectToMongo();
 
-app.listen(3000, ()=>{
-    console.log('serving on port 3000')
-})
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log('serving on port 3000');
+});
